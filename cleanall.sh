df122374c20c3dd8a62889f067e69e5df79ff820
df478 <eledavid88@gmail.com>
2025-11-21 20:12:47 -0400

    first commit

---
+++ cleanall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf backend/.venv
+rm -rf backend/src/__pycache__
+rm -rf backend/src/services/__pycache__
+
+rm -rf frontend/node_modules
+
+echo \\e[32mCleaned all files successfully.\\e[0m
